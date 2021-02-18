ea01e9ff42e81e23af0235ab0febe4d970261091
Andrey Kuksa <andrey20011005@gmail.com>
2021-02-18 18:23:35 +0300

    Stream realization of task

--- src/main/java/by/fpmi/os/main/StreamStringProcessor.java
+++ src/main/java/by/fpmi/os/main/StreamStringProcessor.java
@@ -1,18 +1,35 @@
 package by.fpmi.os.main;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamStringProcessor implements StringProcessor {
     @Override
     public List<String> findFrequentStrings(List<String> strings, int n, int threadsCount) {
         ForkJoinPool threadPool = new ForkJoinPool(threadsCount);
-        Map<String, Long> stringFrequency = new HashMap<>();
-        strings.parallelStream()
-                .forEach(s -> stringFrequency.put(s, Stream.of(s).count()));
-        return null;
+        Map<String, Long> stringFrequency;
+        List<String>res = new ArrayList<>();
+        try {
+            stringFrequency = threadPool.submit(() -> strings.parallelStream()
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())))
+                    .get();
+            List<Map.Entry<String, Long>> result= new ArrayList<>(stringFrequency.entrySet());
+            result.sort((first, second) -> (int) (second.getValue() - first.getValue()));
+            for (int i = 0; i < n; i++) {
+                String string = result.get(i).getKey();
+                res.add(string);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return res;
     }
 }
